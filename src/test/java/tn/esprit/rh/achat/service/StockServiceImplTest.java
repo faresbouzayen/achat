2fd8e1d3eff21f01eb5c0f64e41a99b970f78ed0
Fares Bouzayen <ferisbouzayen@gmail.com>
2024-06-05 17:04:59 +0100

    added tests

--- src/test/java/tn/esprit/rh/achat/service/StockServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/service/StockServiceImplTest.java
@@ -1,113 +1,55 @@
 package tn.esprit.rh.achat.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.stereotype.Service;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tn.esprit.rh.achat.entities.Stock;
 import tn.esprit.rh.achat.repositories.StockRepository;
-import tn.esprit.rh.achat.services.IStockService;
+import tn.esprit.rh.achat.services.StockServiceImpl;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
-@Service
+import static org.mockito.Mockito.*;
+
 @Slf4j
 @SpringBootTest
-@ExtendWith(SpringExtension.class)
-@ExtendWith(MockitoExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-public class StockServiceImplTest implements IStockService {
+@ExtendWith(MockitoExtension.class)
+public class StockServiceImplTest {
 
-	@Autowired
+	@InjectMocks
+	private StockServiceImpl stockService;
+
 	@Mock
-	StockRepository stockRepository;
-
-
-	@Override
-	public List<Stock> retrieveAllStocks() {
-		// récuperer la date à l'instant t1
-		log.info("In method retrieveAllStocks");
-		List<Stock> stocks = (List<Stock>) stockRepository.findAll();
-		for (Stock stock : stocks) {
-			log.info(" Stock : " + stock);
-		}
-		log.info("out of method retrieveAllStocks");
-		// récuperer la date à l'instant t2
-		// temps execution = t2 - t1
-		return stocks;
-	}
-
-	@Override
-	public Stock addStock(Stock s) {
-		// récuperer la date à l'instant t1
-		log.info("In method addStock");
-		return stockRepository.save(s);
-
-	}
-
-	@Override
-	public void deleteStock(Long id) {
-
-	}
+	private StockRepository stockRepository;
 
 	@Test
 	@Order(1)
-	void deleteStock() {
-		Long stockId = 1L; // Provide the value of stockId
-		log.info("In method deleteStock");
-		stockRepository.deleteById(stockId);
+	void testDeleteStock() {
+		Long stockId = 1L;
+		when(stockRepository.existsById(stockId)).thenReturn(true);
+		stockService.deleteStock(stockId);
+		verify(stockRepository, times(1)).deleteById(stockId);
 	}
 
-
-	@Override
-	public Stock updateStock(Stock s) {
-		log.info("In method updateStock");
-		return stockRepository.save(s);
+	@Test
+	@Order(2)
+	void testRetrieveAllStocks() {
+		when(stockRepository.findAll()).thenReturn(List.of(new Stock(), new Stock()));
+		List<Stock> stocks = stockService.retrieveAllStocks();
 	}
 
-	@Override
-	public Stock retrieveStock(Long stockId) {
-		long start = System.currentTimeMillis();
-		log.info("In method retrieveStock");
-		Stock stock = stockRepository.findById(stockId).orElse(null);
-		log.info("out of method retrieveStock");
-		 long elapsedTime = System.currentTimeMillis() - start;
-		log.info("Method execution time: " + elapsedTime + " milliseconds.");
-
-		return stock;
+	@Test
+	@Order(3)
+	void testAddStock() {
+		Stock stockToAdd = new Stock();
+		when(stockRepository.save(stockToAdd)).thenReturn(stockToAdd);
+		Stock savedStock = stockService.addStock(stockToAdd);
 	}
-
-	@Override
-	public String retrieveStatusStock() {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-		Date now = new Date();
-		String msgDate = sdf.format(now);
-		String finalMessage = "";
-		String newLine = System.getProperty("line.separator");
-		List<Stock> stocksEnRouge = (List<Stock>) stockRepository.retrieveStatusStock();
-		for (int i = 0; i < stocksEnRouge.size(); i++) {
-			finalMessage = newLine + finalMessage + msgDate + newLine + ": le stock "
-					+ stocksEnRouge.get(i).getLibelleStock() + " a une quantité de " + stocksEnRouge.get(i).getQte()
-					+ " inférieur à la quantité minimale a ne pas dépasser de " + stocksEnRouge.get(i).getQteMin()
-					+ newLine;
-
-		}
-		log.info(finalMessage);
-		return finalMessage;
-	}
-
 }
